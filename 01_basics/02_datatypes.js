2b8519c6322bbf7c360e33c8496f72663bad7f19
Devansh Dixit <dixitdev016@gmail.com>
2026-01-03 21:12:51 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version;
+              // use strict = no mistakes allowed! (undeclared vars error, no accidental globals)
+              // STRICT MODE: declare every variable, will catch the mistakes
+// alert(3+3) // we are using nodejs , not browser
+ console.log (3
+    + 
+      3) // code readability should be high
+
+ console.log("Devansh")     
+
+ let name = "Devansh"
+ let age = 18
+ let isLoggedIn = false
+ let state;
+
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean =>true/false
+ // null => standalone value
+ // undefined =>
+ // symbol=> unique
+
+
+ //object
+
+ console.log(typeof "Devansh");
+ console.log(typeof age);
+ console.log(typeof null); //important interview question (OUTPUT) --> object
+ console.log(typeof undefined); //importan interview question (OUTPUT) --> undefined
