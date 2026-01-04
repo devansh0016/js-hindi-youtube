a7c16c0d3d91e5cbc1a08de0bfa295fd3e9853ff
Devansh Dixit <dixitdev016@gmail.com>
2026-01-04 08:28:25 +0530

    ConversionOfDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Devansh"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33" => NaN (in this case the type will be number only)
+// true => 1; false => 0
+
+let isLoggedIn = "Devansh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Devansh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
