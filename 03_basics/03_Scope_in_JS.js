dbc2326d738098a4af91f9bf2426a3ebb50ca772
Devansh Dixit <dixitdev016@gmail.com>
2026-01-08 22:28:19 +0530

    Scopes (Modified)

--- 03_basics/03_Scope_in_JS.js
+++ 03_basics/03_Scope_in_JS.js
@@ -4,7 +4,7 @@ if (true) {     // LOCAL SCOPE ,memory is allocated within the braces {} only
 let a = 10
 const b = 20
 var c = 30
-console.log("INNER ", a); // values that are to be taken as an output should be printed within the braces{} only
+console.log("INNER ", a); // values that are to be taken as an output should be printed/accessed within the braces{} only
 
 }
 
